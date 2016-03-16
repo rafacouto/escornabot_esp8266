53c64d505bf281fa75a7172b8230803cd852954e
Rafa Couto <caligari@treboada.net>
2016-03-16 10:03:22 +0100

    HTML skeleton.

---
+++ js/escornabot.js
@@ -0,0 +1,19 @@
+// escornabot.js
+// License: GPLv3 (see LICENSE.txt)
+// (c) 2016 Rafa Couto <caligari@treboada.net>
+
+
+var Keypad = Class.create({
+
+  init: function(id) {
+    this.id = id;
+    this._lookForButtons();
+  },
+
+  _lookForButtons: function() {
+    // ToDo
+  }
+
+});
+
+
